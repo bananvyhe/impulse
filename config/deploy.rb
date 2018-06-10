897448529ccfbc0a1a2a7a63dc0d17afa6780d46
bananvyhe <loadonden@yahoo.com>
2018-06-10 12:39:23 +0500

    `

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,8 +24,8 @@ set :repo_url, "git@github.com:bananvyhe/impulse.git"
  append :linked_files, "config/database.yml"
 
 # Default value for linked_dirs is []
- append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "public/uploads"
- set :shared_children, shared_children + %w{public/uploads}
+ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "public/uploads/ckeditor/pictures"
+
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
