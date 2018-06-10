f4f3f858b7a5c64b678688898c99410cb1f142bf
bananvyhe <loadonden@yahoo.com>
2018-06-10 23:16:19 +0500

    2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,8 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
+  config.assets.precompile += Ckeditor.assets
   config.assets.js_compressor = :uglifier
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
