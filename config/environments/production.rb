a94708fd8bac63c0b645defc4368fe80a27bc54d
bananvyhe <loadonden@yahoo.com>
2018-06-10 23:24:39 +0500

    4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,9 +29,9 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-  config.assets.precompile << Rails.root.join('vendor', 'assets', 'bower_components')
+  
   config.assets.precompile += Ckeditor.assets
-  config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(mangle: false)
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
