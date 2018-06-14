b710f0fbb4fe898f39cd837bd4c38f182a6ff479
bananvyhe <loadonden@yahoo.com>
2018-06-14 14:21:42 +0500

    remove unuse js

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -11,11 +11,10 @@ import App from '../app.vue'
 import Head from '../head.vue'
 import Employees from '../employees.vue'
 // import 'element-ui/lib/theme-chalk/index.css';
-import { Carousel, CarouselItem, Input, Button, Table, TableColumn, Col, Form, FormItem, Pagination, Card } from 'element-ui'
+import { Carousel, CarouselItem, Input, Button, Table, TableColumn, Col, Form, FormItem, Pagination } from 'element-ui'
 Vue.use(CarouselItem) 
 Vue.use(Button) 
 Vue.use(Pagination)
-Vue.use(Card)
 // Vue.use(Input) 
 // Vue.use(Table)
 // Vue.use(TableColumn)
