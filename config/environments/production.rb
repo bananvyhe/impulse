52f91c5bb488d55c99570c6448496db9a1adebe5
bananvyhe <loadonden@yahoo.com>
2018-07-02 18:35:27 +0500

    devise setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
-
+  config.action_mailer.default_url_options = { :host => 'impuls-psy.ru' }
   config.assets.precompile += Ckeditor.assets
   config.assets.precompile += %w( ckeditor/* )
   config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
