cdbf6e27cd71a0786086e89fbd51e36bcd214492
bananvyhe <loadonden@yahoo.com>
2018-07-08 18:06:32 +0500

    reviews sect scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_04_115424) do
+ActiveRecord::Schema.define(version: 2018_07_08_125732) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -105,6 +105,13 @@ ActiveRecord::Schema.define(version: 2018_07_04_115424) do
     t.string "avatar"
     t.integer "cat"
     t.boolean "visible"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "name"
+    t.text "subj"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "sliders", force: :cascade do |t|
