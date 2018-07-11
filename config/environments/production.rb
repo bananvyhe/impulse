b804a4530f4bf1621664e5436d4e2e42e649690c
bananvyhe <loadonden@yahoo.com>
2018-07-11 22:04:12 +0500

    prod setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "impulse_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { :host => 'impuls-psy.ru', :from => 'admin@impuls-psy.ru' }
+  config.action_mailer.default_url_options = { :host => 'impuls-psy.ru', :from => 'deploy@pixeltech.ru' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_caching = false
   config.action_mailer.smtp_settings = {
@@ -77,8 +77,8 @@ Rails.application.configure do
     :port => 25,
     :domain => 'yandex.ru',
     :authentication => :plain,
-    :user_name => 'admin@impuls-psy.ru',
-    :password => '78803054'
+    :user_name => 'deploy@pixeltech.ru',
+    :password => Rails.application.credentials.secret_mail
   }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
