62bed8d74aaf6c3f5ae3fb86f5bdc1de8c057991
bananvyhe <loadonden@yahoo.com>
2018-09-27 10:12:52 +0500

    adding kat module

--- db/migrate/20180927044825_add_column_reviwes.rb
+++ db/migrate/20180927044825_add_column_reviwes.rb
@@ -1,4 +1,6 @@
 class AddColumnReviwes < ActiveRecord::Migration[5.2]
   def change
+  	add_reference :reviews, :katbib, index: true
+  	add_column :reviews, :position, :integer
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_27_043958) do
+ActiveRecord::Schema.define(version: 2018_09_27_044825) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -132,6 +132,9 @@ ActiveRecord::Schema.define(version: 2018_09_27_043958) do
     t.text "subj"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "katbib_id"
+    t.integer "position"
+    t.index ["katbib_id"], name: "index_reviews_on_katbib_id"
   end
 
   create_table "sliders", force: :cascade do |t|
