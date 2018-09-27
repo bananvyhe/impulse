2aac8d63de5266a4900652e35ba1ca9a41624798
bananvyhe <loadonden@yahoo.com>
2018-09-27 18:18:17 +0500

    default kat assigned foe new item library

--- app/models/library.rb
+++ app/models/library.rb
@@ -4,8 +4,12 @@ class Library < ApplicationRecord
 	mount_uploader :cover, CoverUploader
 	attr_accessor :crop_x, :crop_y, :crop_w, :crop_h
   after_update :crop_cover
+  after_initialize :set_default_kat, :if => :new_record?
 acts_as_list scope: :katbib2
 	belongs_to :katbib2
+	def set_default_kat
+    self.katbib2_id ||= 1
+  end
   def crop_cover
     cover.recreate_versions! if crop_x.present?
   end
