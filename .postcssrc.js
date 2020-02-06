9085ac552fce714e2f415604b1a12f2079da5601
bananvyhe <loadonden@yahoo.com>
2020-02-06 18:16:38 +0500

    postcss-loader move typo settings from yml to js
    postcss-loader

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,1 +1,133 @@
+module.exports = {
  
+  map: false,
+  plugins: {
+    'postcss-typography': {
+     
+    baseFontSize: "16px",
+    baseLineHeight: 1.3,  
+    scaleRatio: 2.5,
+    includeNormalize: true,
+
+    },
+ 'postcss-import': {},
+  'postcss-extend': {},
+  'postcss-nested': {},
+  'postcss-simple-vars': {},
+  'postcss-custom-media': {},
+
+  
+  'postcss-cssnext': {features: {'rem': false}},
+  'lost': {},
+  
+  'postcss-assets': {
+    loadPaths: ['images/'],
+    basePath: 'public',
+    cachebuster: true,
+  },
+ 
+  'postcss-font-magician': {
+    custom: {
+      'RobotoCon': {
+        variants: {
+          normal: {
+            300: {
+              url: {
+                woff2: 'fonts/robotocondensed-light-webfont.woff2'
+              }
+            },
+            400: {
+              url: {
+                woff2: 'fonts/robotocondensed-regular-webfont.woff2'
+              }
+            },
+            700: {
+              url: {
+                woff2: 'fonts/robotocondensed-bold-webfont.woff2'
+              }
+            }
+          },
+          italic: {
+            300: {
+              url: {
+                woff2: 'fonts/robotocondensed-lightitalic-webfont.woff2'
+              }
+            },
+            400: {
+              url: {
+                woff2: 'fonts/robotocondensed-italic-webfont.woff2'
+              }
+            },
+            700: {
+              url: {
+                woff2: 'fonts/robotocondensed-bolditalic-webfont.woff2'
+              }
+            }
+          }
+        }
+      },
+      'RobotoA': {
+        variants: {
+          normal: {
+            100: {
+              url: {
+                woff2: 'fonts/roboto-thin-webfont.woff2'
+              }
+            },
+            300: {
+              url: {
+                woff2: 'fonts/roboto-light-webfont.woff2'
+              }
+            },
+            400: {
+              url: {
+                woff2: 'fonts/roboto-regular-webfont.woff2'
+              }
+            },
+            500: {
+              url: {
+                woff2: 'fonts/roboto-medium-webfont.woff2'
+              }
+            },
+            700: {
+              url: {
+                woff2: 'fonts/roboto-bold-webfont.woff2'
+              }
+            },
+            900: {
+              url: {
+                woff2: 'fonts/roboto-black-webfont.woff2'
+              }
+            }
+          },
+          italic: {
+            300: {
+              url: {
+                woff2: 'fonts/roboto-lightitalic-webfont.woff2'
+              }
+            },
+            400: {
+              url: {
+                woff2: 'fonts/roboto-mediumitalic-webfont.woff2'
+              }
+            },
+            700: {
+              url: {
+                woff2: 'fonts/roboto-bolditalic-webfont.woff2'
+              }
+            },
+          }
+        }
+      }
+    },
+    protocol: 'https:'
+  }
+ 
+
+
+  }
+}
+
+
+
+
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,8 +1,9 @@
 const { environment } = require('@rails/webpacker')
 const erb =  require('./loaders/erb')
 const vue =  require('./loaders/vue')
-
+const post =  require('./loaders/post')
 
 environment.loaders.append('vue', vue)
 environment.loaders.append('erb', erb)
+environment.loaders.append('post', post)
 module.exports = environment
